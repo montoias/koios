e835ee50395ebba22605c4f86919a7b50aa7ec70
Alexandre Santos <alexandre.santos@uniplaces.com>
2017-06-02 22:22:38 +0100

    Add home, login and suggestions routes

--- imports/ui/containers/main.js
+++ imports/ui/containers/main.js
@@ -1,11 +1,33 @@
 import React from 'react';
-
 import { createContainer } from 'meteor/react-meteor-data';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Login = () => {
+  return (<div>Login</div>);
+}
+
+const Home = () => {
+  return (<div>Home</div>);
+}
+
+const Suggestions = () => {
+  return (<div>Suggestions</div>);
+}
 
 class Main extends React.Component {
   render() {
     return (
-      <div>Main</div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/suggestions" component={Suggestions}/>
+        </div>
+      </Router>
     );
   }
 }
