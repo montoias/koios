91c505ad2b13c9b7379544d5af691a30735f69c1
Tiago <tiagosardinha94@hotmail.com>
2017-06-03 00:06:14 +0100

    Setup facebook service configuration

---
+++ imports/startup/server/accounts/config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+  service: 'facebook',
+  appId: '713827718819358',
+  secret: '37191c3f3c7a505375e2b37942812c4f'
+});
